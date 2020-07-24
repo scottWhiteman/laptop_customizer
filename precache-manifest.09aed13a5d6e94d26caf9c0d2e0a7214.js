00dbd1d085c62c78fb897953fd6fdf9d858bcc06
Scott <Scott@SCOTT-PC>
2020-07-24 12:42:05 -0700

    Updates

---
+++ precache-manifest.09aed13a5d6e94d26caf9c0d2e0a7214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8483a5dd9d290cfccdbf977c408793",
+    "url": "/laptop-customizer/index.html"
+  },
+  {
+    "revision": "360f2fd3a151a209f28f",
+    "url": "/laptop-customizer/static/css/main.72a41afd.chunk.css"
+  },
+  {
+    "revision": "a3189dd90bd7726e1ec5",
+    "url": "/laptop-customizer/static/js/2.6549ce60.chunk.js"
+  },
+  {
+    "revision": "360f2fd3a151a209f28f",
+    "url": "/laptop-customizer/static/js/main.b4c11b97.chunk.js"
+  },
+  {
+    "revision": "499f4b4adf8f43d881a8",
+    "url": "/laptop-customizer/static/js/runtime~main.9cc997fb.js"
+  }
+]);
